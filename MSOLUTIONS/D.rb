6c7a22f2b93d0e3d877bfb32d79b7f6f60da48f4
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-07-25 23:05:46 +0900

    MSOLUTIONS_contest

--- MSOLUTIONS/D.rb
+++ MSOLUTIONS/D.rb
@@ -1,18 +1,13 @@
-# N = gets.to_i
-# a = gets.split.map &:to_i
-# money = 1000
-# stock = 0
-#
-# for n in 1..N-1
-#   if a[n-1] < a[n]
-#     stock = stock + money / a[n-1] * a[n]
-#     money = money - money / a[n-1] * a[n-1]
-#   elsif a[n-1] > a[n]
-#     stock,money = money,stock
-#   #   money = money + stock / a[n] * a[n-1]
-#   #   stock = stock - stock / a[n] *a[n]
-#   end
-#   # stock = stock * a[n-1]/ a[n]
-# end
-#
-# puts money + stock
+N = gets.to_i
+a = gets.split.map &:to_i
+money = 1000
+stock = 0
+
+for n in 1..N-1
+  if a[n-1] < a[n]
+    stock = money / a[n-1]
+    money = money + (a[n] - a[n-1]) * stock
+  end
+end
+
+puts money
