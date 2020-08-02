98c89f3b52587d37ed7ac75cf5bb93dc50246439
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-08-03 00:10:36 +0900

    add_C.rb

--- ABC_174/B.rb
+++ ABC_174/B.rb
@@ -2,7 +2,7 @@ n,d = gets.split.map(&:to_i)
 count = 0
 n.times do
   x,y = gets.split.map(&:to_i)
-  count += 1 if Math.sqrt(x**2 + y**2) <= d
+  count += 1 if x**2 + y**2 <= d*d
 end
 
 puts count
--- ABC_174/C.rb
+++ ABC_174/C.rb
@@ -1,44 +1,11 @@
-# K = gets.to_i
-#
-# k = K
-# for i in 1..10000000
-#   break puts (i*k).to_s.count("7") unless (k * i).to_s.match(/[^7]/)
-# end
-#
-#
-#
-# # def solve
-#   n = 0
-#   return -1 if K % 2 == 0
-#   (0..999983).each do |i|
-#     n += 7 * (10**i)
-#     next if K > n
-#     return i+1 if n % K == 0
-#   end
-#   -1
-# end
-#
-# puts solve
-
-# require 'prime'
-# res = []
-# sevens = []
-# n = 7
-# seven = ""
-# sevens = []
-# 999982.times do |i|
-#   seven << "7"
-#   sevens << seven.to_i
-# end
-# p sevens
-# sevens.each_with_index do |s,idx|
-#   a = s.prime_division
-#   a.each_with_index do |b|
-#     res[b[0]] ||= idx + 1
-#   end
-# end
-# if res[K].nil?
-#   puts -1
-# else
-#   puts res[K]
-# end
+K = gets.to_i
+x = 7 % K
+i = 0
+K.times do |i|
+  if x == 0
+    puts i+1
+    exit
+  end
+  x = (x*10+7)%K
+end
+puts -1
