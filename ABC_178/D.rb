7232b9f0b53a983203693a481747906da94596a6
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-09-14 00:21:09 +0900

    add_178

--- ABC_178/D.rb
+++ ABC_178/D.rb
@@ -1,12 +1,24 @@
-# TLE!!
-N = gets.to_i
-def solve(m)
-  return $count += 1 if m == 0
-  return if m <= 2
-  (3..m).each do |i|
-    solve(m-i)
+s = gets.to_i
+dp = []
+dp.push(1,0,0)
+(3..s).each do |i|
+  (0..i-3).each do |j|
+    dp[i] ||= 0
+    dp[i] += dp[j]
+    dp[i] %= 10**9+7
   end
 end
-$count = 0
-solve(N)
-puts $count % (10**9 + 7)
+puts dp[s]
+
+# TLE!!
+# N = gets.to_i
+# def solve(m)
+#   return $count += 1 if m == 0
+#   return if m <= 2
+#   (3..m).each do |i|
+#     solve(m-i)
+#   end
+# end
+# $count = 0
+# solve(N)
+# puts $count % (10**9 + 7)
