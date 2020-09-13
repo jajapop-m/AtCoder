79466823f86cb599a0ca8b83cfbd0d485871f2a9
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-09-14 00:40:10 +0900

    add_ABC178

--- ABC_178/D.rb
+++ ABC_178/D.rb
@@ -1,14 +1,25 @@
 s = gets.to_i
-dp = []
-dp.push(1,0,0)
+dp = [0]*(s+1)
+dp[0] = 1
+x = 0
 (3..s).each do |i|
-  (0..i-3).each do |j|
-    dp[i] ||= 0
-    dp[i] += dp[j]
-    dp[i] %= 10**9+7
-  end
+  x += dp[i-3]
+  x %= 10**9+7
+  dp[i] = x
 end
 puts dp[s]
+
+# s = gets.to_i
+# dp = []
+# dp.push(1,0,0)
+# (3..s).each do |i|
+#   (0..i-3).each do |j|
+#     dp[i] ||= 0
+#     dp[i] += dp[j]
+#     dp[i] %= 10**9+7
+#   end
+# end
+# puts dp[s]
 
 # TLE!!
 # N = gets.to_i
