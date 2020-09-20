95983c247a94f528cc70f8f8eacb595174eb9940
jajapop <miki_yudai_19891111@yahoo.co.jp>
2020-09-20 12:06:11 +0900

    add_ABC_179

--- ABC_179/D.rb
+++ ABC_179/D.rb
@@ -1,26 +1,14 @@
-N,k=gets.split.map(&:to_i)
-lr = []
-k.times do
-  l,r=gets.split.map(&:to_i)
-  (l..r).each{|i| lr << i}
-end
-p lr
-dp = Array.new(lr.length){[0]*(N+1)}
-
-lr.each_with_index do |i,idx|
-  if idx==0
-    (1..N).each do |n|
-      break if i*n > N
-      dp[idx][i*n] += 1
-    end
-    next
+n,k=gets.split.map(&:to_i)
+l=k.times.map{gets.split.map(&:to_i)}
+dp=Array.new(n+1,0)
+dp[1]=1
+sum=[0,1]
+(2..n).each do |i|
+  l.each do |a,b|
+    suma=i-b-1>0?sum[i-b-1]:0
+    sumb=i-a>0?sum[i-a]:0
+    dp[i]+=(sumb-suma)%998244353
   end
-
-  (1..N).each do |n|
-    dp[idx][n] = dp[idx-1][n]
-    next if n < i
-    dp[idx][n] += dp[idx-1][n-i] if dp[idx-1][n-i] != 0
-
-  end
+  sum[i]=sum[i-1]+dp[i]
 end
-p dp
+p dp[n]%998244353
--- test.rb
+++ test.rb
@@ -1,2 +1,8 @@
-a,b=`dd`
-puts b+b-a
+case gets.chomp[-1]
+when ?2||?4 || ?5 ||?7||?9
+  puts 'hon'
+when ?3
+  puts 'bon'
+when ?0 || ?1 || ?6 || ?8
+  puts 'pon'
+end
